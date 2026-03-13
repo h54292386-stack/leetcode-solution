97f8aab4725aec14d10b422362d70d353ccd1ebd
h54292386-stack <h54292386@gmail.com>
2026-03-13 09:18:08 +0530

    Time: 214 ms (6.26%), Space: 57.9 MB (26.2%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -4,10 +4,8 @@
  */
 var missingNumber = function(nums) {
     for(let i=0;i<=nums.length;i++){
-    
             if(!nums.includes(i)){
                 return i
-            }
-        
+           }  
     }
 };
